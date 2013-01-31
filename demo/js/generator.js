5772c452b3059f80ec00dfdfc774e4ce1e23303a
Rod Leviton <rod@rodleviton.com>
2013-01-31 05:41:28 -0500

    Added animated pixel projection

--- demo/js/generator.js
+++ demo/js/generator.js
@@ -1,13 +1,13 @@
 $(function() {
 
     var divs = [];
-    var num = 80;
+    var num = 30;
 
     // Generate some random divs
     for (var i = 0; i <= num; i++) {
         divs.push('#div-0' + (i + 1));
         
-        var el = $('<div id="div-0' + (i + 1) + '" class="block"></div>');
+        var el = $('<div id="div-0' + (i + 1) + '" class="block shadow-' + (Math.round(Math.random() * 4) + 1) + '"></div>');
 
         $('#container').append(el);
         position(el);
@@ -17,21 +17,20 @@ $(function() {
 
         
     function position(el) {
-            var element = $(el);
-            var top = Math.round(Math.random() * 960);
-            var left = Math.round(Math.random() * 1441);
-            
-            // Check that element will not be positioned off screen
-            if((top + element.height()) >= 960) {
-                top -= (element.height() * 2);
-            }
-            if((left + element.width()) >= 1441) {
-                left -= (element.width() * 2);
-            }
-            
-            element.css({'top': top, 'left': left, 'position': 'absolute'});
-
+        var element = $(el);
+        var top = Math.round(Math.random() * 640);
+        var left = Math.round(Math.random() * 960);
+        
+        // Check that element will not be positioned off screen
+        if((top + element.height()) >= 640) {
+            top -= (element.height() * 2);
+        }
+        if((left + element.width()) >= 960) {
+            left -= (element.width() * 2);
         }
         
+        element.css({'top': top, 'left': left, 'position': 'absolute'});
+
+    }   
 
 });
--- jquery.pixelprojector.js
+++ jquery.pixelprojector.js
@@ -9,7 +9,7 @@
 (function ( $, window, document, undefined ) {
 
     // Create the defaults once
-    var pixelProjector = "pixelProjector"
+    var pixelProjector = "pixelProjector";
 
     // The actual plugin constructor
     function Plugin( element, options ) {
@@ -24,8 +24,29 @@
     Plugin.prototype = {
 
         init: function() {
-            var topOffset = - $(this.element).offset().top;
-            var leftOffset = - $(this.element).offset().left;
+            var that = this;
+            this.shufflePixels();
+            setInterval(function() { that.generatePosition(); }, 10000);
+        },
+        
+        generatePosition: function() {
+            var that = this;
+            var top = Math.round(Math.random() * 640);
+            var left = Math.round(Math.random() * 960);
+            
+            // Check that element will not be positioned off screen
+            if( (top + $(this.element).height()) >= 640 ) {
+                top -= ( $(this.element).height() * 2 );
+            }
+            if( (left + $(this.element).width()) >= 960 ) {
+                left -= ( $(this.element).width() * 2 );
+            }
+            $(this.element).animate({'top': top, 'left': left, 'position': 'absolute'}, { duration: 1000, step: function() { that.shufflePixels() }} );
+        },
+        
+        shufflePixels: function() {
+            var topOffset = - $(this.element).position().top;
+            var leftOffset = - $(this.element).position().left;
             var offset = (leftOffset + 'px' + ' ' + topOffset + 'px');
             $(this.element).css('background-position', offset);
         }
